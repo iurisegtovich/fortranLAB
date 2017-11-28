a89fb3d02cf05a251d0e341f1bde95891448cb22
iurisegtovich <iurisegtovich@gmail.com>
2017-11-28 15:55:49 -0200

    crate cfuncs because i should know how to pass stuff between c and c before passing them to fortran

--- 0. fortran-c_interoperability/passing-args/c_calls_fortran/2-array[1]/main.c
+++ 0. fortran-c_interoperability/passing-args/c_calls_fortran/2-array[1]/main.c
@@ -8,10 +8,19 @@ void ffunc_b(int b);
 void ffunc_bb(int b[]);
 void ffunc_bbb(int (*b)[1]);
 
+void cfunc_a(int a){}
+void cfunc_aa(int *a){}
+void cfunc_aaa(int **a){}
+void cfunc_b(int b){}
+void cfunc_bb(int b[]){}
+void cfunc_bbb(int (*b)[1]){}
+
 void main(){
 
 int *a=(int *)calloc(1,sizeof(int));
+
 *a=1;
+
 int b[]={2};
 
 printf("\n%p\n",&a);
@@ -19,9 +28,18 @@ printf("\n%p\n",a);
 printf("\n%d\n",*a);
 printf("\n%d\n",a[0]);
 
-ffunc_a(*a);
-ffunc_aa(a);
-ffunc_aaa(&a);
+cfunc_a(*a);
+cfunc_aa(a);
+cfunc_aaa(&a);
+
+printf("\n%p\n",&a);
+printf("\n%p\n",a);
+printf("\n%d\n",*a);
+printf("\n%d\n",a[0]);
+
+/*ffunc_a(*a);*/
+/*ffunc_aa(a);*/
+/*ffunc_aaa(&a);*/
 
 printf("\n%p\n",&a);
 printf("\n%p\n",a);
@@ -35,9 +53,18 @@ printf("\n%p\n",b);
 printf("\n%d\n",*b);
 printf("\n%d\n",b[0]);
 
-ffunc_b(*b);
-ffunc_bb(b);
-ffunc_bbb(&b);
+cfunc_b(*b);
+cfunc_bb(b);
+cfunc_bbb(&b);
+
+printf("\n%p\n",&a);
+printf("\n%p\n",a);
+printf("\n%d\n",*a);
+printf("\n%d\n",a[0]);
+
+/*ffunc_b(*b);*/
+/*ffunc_bb(b);*/
+/*ffunc_bbb(&b);*/
 
 printf("\n%p\n",&b);
 printf("\n%p\n",b);
